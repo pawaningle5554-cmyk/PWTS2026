c427ae31e9c8de2df84e468f1e3cd880db13c4ae
Pawan Ingle <pawaningle5554@gmail.com>
2026-08-11 18:35:34 +0530

    gihubInfo

---
+++ tests/github.spec.ts
@@ -0,0 +1,39 @@
+// Github/gitlab/bitbucket - Version Control Tool
+
+// To work with github/bitbucket/gitlab, we need to have "git" installed in our local
+// We can download git from https://git-scm.com/install/windows and install in local
+
+// git - Tool used to track the code changes
+// github - Online Platform where git repository are stored and helps us in terms 
+// of collaboration
+
+
+// 1. You are the first employee to start test automation - NO REPO AVAILBLE
+
+    // 0. Create a new repository on the version control tool
+    // 1. Create a project and write the test cases inside your local system
+    // 2. "git init" - Initialize the a new got repo inside your local system
+    // 3. "git add ." - Add all the files that you have changed/added in your
+    // local git area
+    // 4. "git commit -m "first commit" " - Commit all the changes to your local
+    // git with the message
+    // 5. "git branch -M master" - Create a new branch named as "master"
+    // 6. "git branch" - List all the branches that in your local repo
+    // 7. "git remote add origin https://github.com/Praveenroy05/gitdemo.git" - Add
+    // a remote repository to your local repo and link it with the remote repo
+    // 8. "git push -u origin master" - Push the commited changes from local "master"
+    // branch to remote repository inside the master branch
+
+    // 9. "git checkout master" - Move to the master branch and pull the latest changes
+    // 10. "git pull" - It will pull all the latest changes from the remote repo tp
+    // local repo
+
+    // 11. git checkout -b branchName - Create a new branch going forward for all the new task
+    // 12 . Complete writing the test case and follow the steps from #3, #4 and #8(change the branch name)
+
+
+
+
+
+
+// 2. If the Repository is already available
